95b020ad28b841c3afb8e578ca4362071188a0b4
MWs <jarekdechaa@gmail.com>
2023-08-08 21:24:20 +0200

    Private class for registration added

--- src/main/java/pageObjectModel/InitPage.java
+++ src/main/java/pageObjectModel/InitPage.java
@@ -1,5 +1,9 @@
 package pageObjectModel;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,6 +21,36 @@ public class InitPage extends MainTools{
 		PageFactory.initElements(this.myDriver, this);
 	}
 	
+	private class Registration {
+		
+		WebDriver regDriver;
+		
+		public Registration (WebDriver drv) {
+			this.regDriver = drv;
+			PageFactory.initElements(this.regDriver, this);
+			System.out.println("Utworzyl");
+		}
+		
+		@FindBy (css = ".col-md-6 input[class*=form]")  // "//form //div[contains(@class, 'col-md-6')] //input[contains(@class, 'form-control')]"
+		List<WebElement> textFieldsList;
+		
+		public void fillTextFields(HashMap<String, String> mapka) {
+			
+			String labelText;
+			System.out.println("Jest we funkcji" + textFieldsList.size());
+			for(WebElement ele : textFieldsList) {
+				labelText = ele.findElement(By.xpath("parent::*/label")).getText();
+				System.out.println(labelText);
+				if(mapka.containsKey(labelText)) {
+					ele.sendKeys(mapka.get(labelText));
+				}
+			}
+			
+		}
+	}
+	
+	
+	
 	@FindBy(css = "input#userEmail")
 	WebElement userNameElement; 
 	
@@ -26,6 +60,14 @@ public class InitPage extends MainTools{
 	@FindBy(css = "a[class*=link]")
 	WebElement forgotPasswordLinkElement;
 	
-//abc
-//cde
+	@FindBy (css = "p[class*=wrapper-footer]")
+	WebElement registerLinkTextElement;
+	
+	
+	public void goToGegistrationSubView(HashMap<String, String> mappp) {
+		registerLinkTextElement.click();
+		Registration regObj = new Registration(this.myDriver);
+		regObj.fillTextFields(mappp);
+	}
+
 }
--- src/test/java/tests/WelcomePage.java
+++ src/test/java/tests/WelcomePage.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
@@ -8,13 +11,14 @@ import net.bytebuddy.implementation.bind.annotation.Super;
 import pageObjectModel.InitPage;
 
 public class WelcomePage extends DriverManager{
+	HashMap<String, String> regStrings = new HashMap<String, String>(Map.of("First Name", "Bajo", "Last Name", "Jajo"));
 	
 
 	@Test
 	public void checkWrongCredentials() {
 	InitPage wPageObj = new InitPage(DriverManager.myDriver);
 	wPageObj.getPage("https://rahulshettyacademy.com/client");
-
+	wPageObj.goToGegistrationSubView(regStrings);
 //		
 	}
 
